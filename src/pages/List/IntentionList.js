feac5fb667ced3d7353ce118f4212e7e08bdb914
hanmengtong <mengtong.han@newtmc.com>
2019-11-06 14:43:01 +0800

    修改意向列表内容及样式 [intentionlist]

--- src/pages/List/IntentionList.js
+++ src/pages/List/IntentionList.js
@@ -41,7 +41,6 @@ const device = ['swing', 'RS5', 'SWING HD', 'dental plus'];
   rule,
   loading: loading.models.rule,
 }))
-
 @Form.create()
 class TableList extends PureComponent {
   state = {
@@ -57,31 +56,36 @@ class TableList extends PureComponent {
     {
       title: '跟进结束日期',
       dataIndex: 'followEndDate',
+      align: 'center',
       sorter: true,
       render: val => <span>{moment(val).format('YYYY-MM-DD HH:mm:ss')}</span>,
     },
     {
       title: '客户名称',
       dataIndex: 'desc',
+      align: 'center',
     },
     {
       title: '联系人',
       dataIndex: 'contacts',
+      align: 'center',
     },
     {
       title: '联系电话',
       dataIndex: 'callNum',
-      align: 'right',
+      align: 'center',
       render: val => `${val} `,
     },
     {
       title: '来源',
       dataIndex: 'relationShip',
+      align: 'center',
     },
     {
-    	title: '意向设备',
+      title: '意向设备',
       dataIndex: 'device',
-      render: val => <span>{device[val]}</span>,//静态数据展示，之后改成可筛选的
+      align: 'center',
+      render: val => <span>{device[val]}</span>, //静态数据展示，之后改成可筛选的
       //render(val) {
       //  return <Badge device={[val]} text={device[val]} />;
       //},
@@ -89,23 +93,25 @@ class TableList extends PureComponent {
     {
       title: '已跟进次数',
       dataIndex: 'followTime',
+      align: 'center',
     },
     {
       title: '逾期次数',
       dataIndex: 'overdueTime',
+      align: 'center',
     },
     {
       title: '操作',
+      align: 'center',
       render: (text, record) => (
         <Fragment>
           <a href="/account/settings/record">记录跟进结果</a>
           <Divider type="vertical" />
-          <a href="/profile/details">查看</a>
+          <a href="/profile/details">查看详情</a>
         </Fragment>
       ),
     },
   ];
-
 
   componentDidMount() {
     const { dispatch } = this.props;
@@ -114,16 +120,14 @@ class TableList extends PureComponent {
     });
   }
 
-
-handleStandardTableChange = (pagination, filtersArg, sorter) => {
-	
+  handleStandardTableChange = (pagination, filtersArg, sorter) => {
     const { dispatch } = this.props;
     const { formValues } = this.state;
     const filters = Object.keys(filtersArg).reduce((obj, key) => {
-    const newObj = { ...obj };
-    newObj[key] = getValue(filtersArg[key]);
-    return newObj;
-}, {});
+      const newObj = { ...obj };
+      newObj[key] = getValue(filtersArg[key]);
+      return newObj;
+    }, {});
 
     const params = {
       currentPage: pagination.current,
@@ -134,19 +138,11 @@ handleStandardTableChange = (pagination, filtersArg, sorter) => {
     if (sorter.field) {
       params.sorter = `${sorter.field}_${sorter.order}`;
     }
-dispatch({
+    dispatch({
       type: 'rule/fetch',
       payload: params,
-});
-    
-    
-};
-
-
-
-
-
-
+    });
+  };
 
   handleFormReset = () => {
     const { form, dispatch } = this.props;
@@ -160,18 +156,13 @@ dispatch({
     });
   };
 
-
-
-
-
-
   toggleForm = () => {
     const { expandForm } = this.state;
     this.setState({
       expandForm: !expandForm,
     });
   };
-/*删除*/
+  /*删除*/
   handleMenuClick = e => {
     const { dispatch } = this.props;
     const { selectedRows } = this.state;
@@ -195,17 +186,13 @@ dispatch({
     }
   };
 
-
- handleSelectRows = rows => {
+  handleSelectRows = rows => {
     this.setState({
       selectedRows: rows,
     });
- };
- 
- 
- 
- 
- /*点击查询*/
+  };
+
+  /*点击查询*/
 
   handleSearch = e => {
     e.preventDefault();
@@ -225,19 +212,16 @@ dispatch({
       });
     });
   };
-  
-  
-  
+
   renderSimpleForm() {
     const {
       form: { getFieldDecorator },
     } = this.props;
-    
+
     return (
-      
       <Form onSubmit={this.handleSearch} layout="inline">
         <Row gutter={{ md: 6, lg: 22, xl: 48 }}>
-        	<Col md={6} sm={22}>
+          <Col md={6} sm={22}>
             <FormItem label="跟进日期">
               {getFieldDecorator('date')(
                 <DatePicker style={{ width: '100%' }} placeholder="请输入跟进日期" />
@@ -286,8 +270,6 @@ dispatch({
     );
   }
 
-
-
   renderForm() {
     const { expandForm } = this.state;
     return expandForm ? this.renderAdvancedForm() : this.renderSimpleForm();
@@ -306,7 +288,6 @@ dispatch({
       </Menu>
     );
 
-   
     return (
       <PageHeaderWrapper>
         <Card bordered={false}>
@@ -315,7 +296,6 @@ dispatch({
             <div className={styles.tableListOperator}>
               {selectedRows.length > 0 && (
                 <span>
-                  
                   <Dropdown overlay={menu}>
                     <Button>
                       更多操作 <Icon type="down" />
@@ -334,7 +314,6 @@ dispatch({
             />
           </div>
         </Card>
-      
       </PageHeaderWrapper>
     );
   }
